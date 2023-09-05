4c9f4723a2f753424dd16ff0a482dff2367d1afa
MSI-PC <zaizai000816@gmail.com>
2023-09-05 14:52:53 +0800

    Modified

--- ValidationHarness.java
+++ ValidationHarness.java
@@ -1,13 +1,13 @@
 import static org.mockito.Mockito.*;
 import org.sosy_lab.sv_benchmarks.Verifier;
 
- public class ValidationHarness {
-     public static void main ( String [] args ) {
-         mockStatic(Verifier.class);
+public class ValidationHarness {
+    public static void main ( String [] args ) {
+        mockStatic(Verifier.class);
         
-         // The statement is inserted here
-         when(Verifier.nondetBoolean()).thenReturn(false);
+        // The statement is inserted here
+        when(Verifier.Type()).thenReturn(true);            
         
-         StringValueOf04.main(new String [0]);
-     }
- }
+        StringValueOf04.main(new String [0]);
+    }
+}
--- lala.py
+++
@@ -1,3 +0,0 @@
-import random
- 
-print(float(random.randint(-2^-149,3.4028235E38)))
